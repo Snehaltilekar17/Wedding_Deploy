4c910f12fefb401ec37fdc6aa261f7072fa289bf
Snehaltilekar17 <142857282+Snehaltilekar17@users.noreply.github.com>
2024-06-05 11:55:12 +0530

    Added "index.php" to repository

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Font Awesome Link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <!-- Custom css file Link -->
+    <link rel="stylesheet" href="css/style.css">
+
+
+
+    <title>Wedding Planner</title>
+</head>
+<body>
+    
+<div class="container">
+<?php
+   @include 'header.php';
+?>
+</div>
+
+
+
+
+
+
+
+
+
+    <!-- custom js file Link -->
+<script src="js/script.js"></script>
+</body>
+</html>
