deef07233e25941262fe442dd358f198d95ba4c6
Snehaltilekar17 <142857282+Snehaltilekar17@users.noreply.github.com>
2024-06-05 12:06:03 +0530

    Uploading remaining files and folders

---
+++ header.php
@@ -0,0 +1,12 @@
+<section class="header">
+    <a href="index.php" class="logo">weeding</a>
+    <nav class="navbar">
+        <a href="index.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="portfolio.php">Portfolio</a>
+        <a href="pricing.php">Pricing</a>
+        <a href="contact.php">Contact</a>
+
+    </nav>
+    <div id="menu-btn" class="fas fa-bars"></div>
+</section>
